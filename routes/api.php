22849e71a5e29e385fc09baf035b763ed63e3e80
zsleader <522376982@qq.com>
2018-03-15 05:54:13 +0000

    socials api

---
+++ routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+$api = app('Dingo\Api\Routing\Router');
+
+$api->version('v1',[
+    'namespace' => 'App\Http\Controllers\Api'
+], function($api){
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function($api){
+        //平台方通过换取access_token
+        $api->post('socails/authorizations', 'authorizationsController@socialStore')
+            ->name('api.socails.authorizations.store');
+
+
+    });
+});
